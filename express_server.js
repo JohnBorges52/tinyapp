1875afd5495841cfddceb9004a05e2a59db3ea46
John <jrborges52@email.com>
2022-06-15 04:33:12 +0000

    finished the part with logged in

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const router = express.Router();
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
+app.use(cookieParser());
 
 
 const urlDatabase = {
@@ -14,27 +16,25 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-
-
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {  username: req.cookies["username"], urls: urlDatabase };
+  res.render("urls_new", templateVars);
 });
 
 ///// GO TO THE PAGE WITH ALL OF THE URLS SAVED
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 //// CREATE A SHORT SPECIFIC SHORT URL 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
@@ -53,7 +53,7 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-//// CREATE A DELETE FUNCTION T
+//// CREATE A DELETE FUNCTION 
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
@@ -71,7 +71,7 @@ app.post("/urls/:shortURL/edit", (req,res) => {
 })
 
 app.get("/urls/:shortURL/edit", (req,res) => {
- const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+ const templateVars = { username: req.cookies["username"],shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars)
 })
 
@@ -81,8 +81,9 @@ app.post("/urls/login", (req,res) => {
 
   const username = req.body.username
   console.log(username)
-  res.cookie(username)
-
+  res.cookie("username", username)
+  console.log(req.cookies)
+  console.log(req.body)
   res.redirect("/urls")
 
 
